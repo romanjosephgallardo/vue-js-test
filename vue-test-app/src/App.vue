6f5d2eed86fcfee4d4d04184e6c1e37b6f7a306d
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2025-08-10 10:05:37 +0800

    reactive() test.

--- vue-test-app/src/App.vue
+++ vue-test-app/src/App.vue
@@ -1,41 +1,12 @@
-<script>
-export default {
-	data(){
-		return {
-			name: 'Jace',
-			status: 'running', // Status: Sleeping, Running, Idle
-			tasks: ['Task 1', 'Task 2', 'Task 3' ],
-			link: 'https://google.com'
-		};
-	},
-
-	methods: {
-		toggleStatus() {
-			if (this.status === 'active'){
-				this.status = 'pending';
-			} else if (this.status === 'pending'){
-				this.status = 'inactive';
-			} else {
-				this.status = 'active';
-			}
-		}
-	}
-};
+<script setup>
+    import { reactive } from 'vue';
+    let initialState = reactive({ count: 0 });
 </script>
 
 <template>
-	<h1>Hi {{ name }}! You did open this site!</h1>
-	<p v-if="status === 'pending'"> Sleeping! </p>
-	<p v-else-if="status === 'running'"> Running! </p>
-	<p v-else="status === 'idle'"> Idle! </p>
-
-	<h3> Tasks:</h3>
-	<ul>
-		<li v-for="task in tasks" key="task"> {{ task }} </li>
-	</ul>
-	<a :href="link"> Click for Google! </a>
-	</br>
-	<button @:click="toggleStatus"> Change Status </button>
+    <h1> Count: {{  initialState.count  }}</h1>
+	<button @click="initialState.count++"> Increment </button>
+	<button @click="initialState.count--"> Decrement </button>
 </template>
-
+ 
 <style></style>
